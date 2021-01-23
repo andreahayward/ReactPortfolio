75331b0fff9781b0b6baea5a865deab8df4fd680
Andrea Hayward <andrea.teofilo@yahoo.com>
2021-01-23 14:09:07 -0500

    adding resume link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,6 +9,7 @@ function Contact()
         <h2 className="text-center"><a href="https://www.linkedin.com/in/andrea-hayward-251ba769/"> LinkedIn </a></h2>
         <h2 className="text-center"><a href="https://github.com/andreahayward"> GitHub </a></h2>
         <h3 className="text-center"><a href="https://www.instagram.com/andreeuhhhh"> Instagram </a></h3>
+        <h3 className="text-center"><a href="https://docs.google.com/document/d/1o8X0YDKedlcTfvKiE2qdo7qRn5F-AY99y6F2UrBvARA/edit?usp=sharing"> Resume </a></h3>
 
       </div>
     );
